3afcce4b0a99c46a00ede887819efc27a869242f
msmHvupham <vu.1xkc.phamhoai@misumi.co.jp>
2025-12-12 14:23:52 +0900

    fix client

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,17 +1,12 @@
 import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data'; // Remove fetchLatestInvoices
 import { Suspense } from 'react';
 import {
-    RevenueChartSkeleton,
-    LatestInvoicesSkeleton,
     CardsSkeleton,
 } from '@/app/ui/skeletons';
 
 export default async function Page() {
-    // Remove `const latestInvoices = await fetchLatestInvoices()`
     const {
         numberOfInvoices,
         numberOfCustomers,
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,3 +1,4 @@
+
 import {
   BanknotesIcon,
   ClockIcon,
