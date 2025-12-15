36e3cf904992da64f950510ca2d463e933eefeda
msmHvupham <vu.1xkc.phamhoai@misumi.co.jp>
2025-12-15 11:30:43 +0900

    fix e db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
+import { unstable_noStore as noStore } from 'next/cache';
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchRevenue, fetchCardData } from '@/app/lib/data';
 export default async function Page() {
-    const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
+    noStore();
+    // const revenue = await fetchRevenue();
+    // const latestInvoices = await fetchLatestInvoices();
     const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
     return (
         <main>
